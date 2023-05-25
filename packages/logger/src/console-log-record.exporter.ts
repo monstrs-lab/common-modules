2055bb36e2a7284a1674482c29e8762c36c42118
Andrey Linko <AndreyLinko@gmail.com>
2023-05-25 11:56:51 +0300

    feat: logger console in test env

---
+++ packages/logger/src/console-log-record.exporter.ts
@@ -0,0 +1,23 @@
+import type { ReadableLogRecord }                                   from '@opentelemetry/sdk-logs'
+import type { ExportResult }                                        from '@opentelemetry/core'
+
+import { ConsoleLogRecordExporter as BaseConsoleLogRecordExporter } from '@opentelemetry/sdk-logs'
+import { ExportResultCode }                                         from '@opentelemetry/core'
+
+export class ConsoleLogRecordExporter extends BaseConsoleLogRecordExporter {
+  public export(logs: Array<ReadableLogRecord>, resultCallback: (result: ExportResult) => void) {
+    this.sendLogRecords(logs, resultCallback)
+  }
+
+  private sendLogRecords(
+    logRecords: Array<ReadableLogRecord>,
+    done?: (result: ExportResult) => void
+  ): void {
+    for (const logRecord of logRecords) {
+      // @ts-expect-error
+      console.log(JSON.stringify(this._exportInfo(logRecord))) // eslint-disable-line
+    }
+
+    done?.({ code: ExportResultCode.SUCCESS })
+  }
+}
