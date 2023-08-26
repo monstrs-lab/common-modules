049b20c939a0260ca17641b1cb3231f56e3462db
Andrey Linko <AndreyLinko@gmail.com>
2023-08-26 11:35:38 +0300

    feat: guard clause json schema

---
+++ packages/guard-clause/src/extensions/not-json-schema-valid.extension.factory.ts
@@ -0,0 +1,32 @@
+import type { AnySchema }                            from 'ajv'
+
+import type { AbstractGuardExtensionFactoryOptions } from '../factory/index.js'
+
+import AjvPkg                                        from 'ajv'
+
+import { AbstractGuardExtensionFactory }             from '../factory/index.js'
+import { GuardError }                                from '../errors/index.js'
+
+const Ajv = AjvPkg.default || AjvPkg
+
+export class NotJsonSchemaValidGuardExtensionFactory extends AbstractGuardExtensionFactory {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  override performParamValue(paramValue: any, options: AbstractGuardExtensionFactoryOptions): void {
+    const ajv = new Ajv()
+
+    if (options.metadata!.defs) {
+      ajv.addSchema(options.metadata!.defs as AnySchema)
+    }
+
+    const validate = ajv.compile(options.metadata!.schema)
+
+    if (!validate(paramValue)) {
+      throw new GuardError(
+        'guard.against.not-json-schema-valid',
+        options.parameter,
+        paramValue,
+        'not json schema valid'
+      )
+    }
+  }
+}
