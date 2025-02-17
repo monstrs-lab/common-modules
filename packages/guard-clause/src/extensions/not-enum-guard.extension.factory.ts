1ad150a921c34cb52c19fd80760dfb9a5d836073
Andrey Linko <AndreyLinko@gmail.com>
2025-02-17 21:04:34 +0300

    feat(common): guards enum and instance multiple values

--- packages/guard-clause/src/extensions/not-enum-guard.extension.factory.ts
+++ packages/guard-clause/src/extensions/not-enum-guard.extension.factory.ts
@@ -6,15 +6,25 @@ import { AbstractGuardExtensionFactory }             from '../factory/index.js'
 export class NotEnumGuardExtensionFactory extends AbstractGuardExtensionFactory {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   override performParamValue(paramValue: any, options: AbstractGuardExtensionFactoryOptions): void {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    if (!Object.values(options.metadata!.targetEnum).includes(paramValue)) {
+    const targetEnums = Array.isArray(options.metadata!.targetEnum)
+      ? options.metadata!.targetEnum
+      : [options.metadata!.targetEnum]
+
+    const isMatched = targetEnums.some(
+      (targetEnum) =>
+        targetEnum &&
+        typeof targetEnum === 'object' &&
+        Object.values(targetEnum as object).includes(paramValue)
+    )
+
+    if (!isMatched) {
       throw new GuardError(
         'guard.against.not-enum',
         options.parameter,
         paramValue,
-        `not enum value of '${
-          (options.metadata!.targetEnum?.name || options.metadata!.targetEnum) as string
-        }'`
+        `not enum value of '${targetEnums
+          .map((targetEnum) => (targetEnum?.name as string) || (targetEnum as string))
+          .join(' or ')}'`
       )
     }
   }
